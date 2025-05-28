ad0f3ab811f6ba57ded7067c9c4057951f1d660f
Samuel Wu <twopizza9621536@gmail.com>
2025-05-28 12:13:15 -0400

    fix: 🩹 Fix disk partition configuration
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- modules/hosts/amitie/disko.nix
+++ modules/hosts/amitie/disko.nix
@@ -1,36 +1,38 @@
 {
-  flake.modules.nixos.amitie.disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partations = {
-            ESP = {
-              type = "EF00";
-              size = "512M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/efi";
-                mountOptions = ["umask=0077"];
+  flake.modules.nixos.amitie = {
+    disko.devices = {
+      disk = {
+        main = {
+          device = "/dev/sda";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                type = "EF00";
+                size = "512M";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/efi";
+                  mountOptions = ["umask=0077"];
+                };
               };
-            };
-            root = {
-              end = "-8G";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+              root = {
+                end = "-8G";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
               };
-            };
-            swap = {
-              size = "100%";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true;
+              swap = {
+                size = "100%";
+                content = {
+                  type = "swap";
+                  discardPolicy = "both";
+                  resumeDevice = true;
+                };
               };
             };
           };
