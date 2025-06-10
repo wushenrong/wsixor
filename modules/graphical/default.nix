58615e49198923622ee2ae38f131a66fe4dbfaed
Samuel Wu <twopizza9621536@gmail.com>
2025-06-10 11:54:39 -0400

    feat: ✨ Add more xdg packages
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -32,6 +32,10 @@
       jack.enable = true;
     };
 
-    xdg.portal.enable = true;
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    };
   };
 }
