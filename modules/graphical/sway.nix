d7dabebd70bd80c25eb8d85756775e817dc876dd
Samuel Wu <twopizza9621536@gmail.com>
2025-06-10 11:13:39 -0400

    feat: ✨ Merge greetd and sway modules
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- modules/graphical/sway.nix
+++ modules/graphical/sway.nix
@@ -2,8 +2,52 @@
 #
 # SPDX-License-Identifier: MIT-0
 {
-  unify.modules.sway.nixos = {
+  unify.modules.sway.nixos = {pkgs, ...}: let
+    swayConfig = pkgs.writeText "greetd-sway-config" ''
+      exec "${pkgs.greetd.regreet}/bin/regreet; swaymsg exit"
+      bindsym Mod4+shift+e exec swaynag \
+        -t warning \
+        -m 'What do you want to do?' \
+        -b 'Poweroff' 'systemctl poweroff' \
+        -b 'Reboot' 'systemctl reboot'
+      include /etc/sway/config.d/*
+    '';
+  in {
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+          user = "greeter";
+        };
+      };
+    };
+
     programs = {
+      regreet = {
+        enable = true;
+        settings = {
+          background = {
+            path = "/home/samgo/Pictures/wallpaper.jpg";
+          };
+          #     commands = {
+          #       reboot = ["systemctl" "reboot"];
+          #       poweroff = ["systemctl" "poweroff"];
+          #     };
+          #   };
+          #   theme = {
+          #     name = "Matcha-aliz";
+          #     package = pkgs.matcha-gtk-theme;
+          #   };
+          #   iconTheme = {
+          #     name = "Papirus";
+          #     package = pkgs.papirus-icon-theme;
+          #   };
+          #   cursorTheme = {
+          #     name = "Papirus";
+          #     package = pkgs.papirus-icon-theme;
+        };
+      };
       sway = {
         enable = true;
         wrapperFeatures.gtk = true;
