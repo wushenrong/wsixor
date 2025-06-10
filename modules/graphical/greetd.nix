aefa63669290f1bc467a680123473798b4b471af
Samuel Wu <twopizza9621536@gmail.com>
2025-06-09 23:14:45 -0400

    fix: 🩹 Use default compositor for regreet
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- modules/graphical/greetd.nix
+++ modules/graphical/greetd.nix
@@ -2,19 +2,7 @@
 #
 # SPDX-License-Identifier: MIT-0
 {
-  unify.modules.greetd.nixos = {pkgs, ...}: let
-    swayConfig = pkgs.writeText "greetd-sway-config" ''
-      exec "${pkgs.greetd.regreet}/bin/regreet; swaymsg exit"
-
-      bindsym Mod4+shift+e exec swaynag \
-        -t warning \
-        -m 'What do you want to do?' \
-        -b 'Poweroff' 'systemctl poweroff' \
-        -b 'Reboot' 'systemctl reboot'
-
-      include /etc/sway/config.d/*
-    '';
-  in {
+  unify.modules.greetd.nixos = {pkgs, ...}: {
     programs.regreet = {
       enable = true;
       settings.commands = {
@@ -35,16 +23,6 @@
       };
     };
 
-    environment.systemPackages = [pkgs.sway];
-
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
-          user = "greeter";
-        };
-      };
-    };
+    services.greetd.enable = true;
   };
 }
