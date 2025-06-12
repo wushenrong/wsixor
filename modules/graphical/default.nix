e42d938167f61471e47e20867bc4508ae9201e5a
Samuel Wu <twopizza9621536@gmail.com>
2025-06-11 20:36:46 -0400

    feat: ✨ Add adwaita icons as fallback
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -11,6 +11,7 @@
     environment = {
       sessionVariables.NIXOS_OZONE_WL = "1";
       systemPackages = with pkgs; [
+        adwaita-icon-theme
         glib
         kitty
         matcha-gtk-theme
